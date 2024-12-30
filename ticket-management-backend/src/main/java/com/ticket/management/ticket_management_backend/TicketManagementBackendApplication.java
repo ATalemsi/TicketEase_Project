1d7e3b9acdb3ac4a4512b247d0e005c71eb37ee7
ATalemsi <mohamadtalemsi@gmail.com>
2024-12-30 10:21:33 +0100

    first commit

---
+++ ticket-management-backend/src/main/java/com/ticket/management/ticket_management_backend/TicketManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.management.ticket_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagementBackendApplication.class, args);
+	}
+
+}
---
+++ ticket-management-backend/src/test/java/com/ticket/management/ticket_management_backend/TicketManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.management.ticket_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
