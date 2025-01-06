c8b9db39f408ba1d916553a23e225a48bd3ed67d
ATalemsi <mohamadtalemsi@gmail.com>
2025-01-06 10:20:50 +0100

    structure de la partie front-end

---
+++ ticket-management-frontend/src/app/features/agent/ticket-queue/ticket-queue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-queue',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-queue.component.html',
+  styleUrl: './ticket-queue.component.css'
+})
+export class TicketQueueComponent {
+
+}
---
+++ ticket-management-frontend/src/app/features/agent/ticket-response/ticket-response.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-response',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-response.component.html',
+  styleUrl: './ticket-response.component.css'
+})
+export class TicketResponseComponent {
+
+}
